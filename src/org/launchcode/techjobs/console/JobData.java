2baad1ebecb3aba631abb9936948d1595a290e99
Anu Kuchi <apkuchi@gmail.com>
2020-05-04 21:04:38 -0500

    Upload all the files

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -10,6 +10,7 @@ import java.io.Reader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by LaunchCode
@@ -62,7 +63,7 @@ public class JobData {
      * with "Enterprise Holdings, Inc".
      *
      * @param column   Column that should be searched.
-     * @param value Value of teh field to search for
+     * @param value Value of the field to search for
      * @return List of all jobs matching the criteria
      */
     public static ArrayList<HashMap<String, String>> findByColumnAndValue(String column, String value) {
@@ -74,14 +75,31 @@ public class JobData {
 
         for (HashMap<String, String> row : allJobs) {
 
-            String aValue = row.get(column);
+            String aValue = row.get(column).toLowerCase();
 
-            if (aValue.contains(value)) {
+            if (aValue.contains(value.toLowerCase())) {
                 jobs.add(row);
             }
         }
 
         return jobs;
+    }
+
+    public static ArrayList<HashMap<String, String>> findByValue(String value) {
+
+            loadData();
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+        for (HashMap<String, String> postInfo : allJobs) {
+            for (Map.Entry<String, String> career : postInfo.entrySet()) {
+                if(jobs.contains(career)){
+                    jobs.add(postInfo);
+                }
+                if (career.getValue().toLowerCase().contains(value.toLowerCase())) {
+                    jobs.add(postInfo); }
+            }
+        } return jobs;
+
     }
 
     /**
